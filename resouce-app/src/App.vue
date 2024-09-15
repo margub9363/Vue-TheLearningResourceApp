0d36058487d0c5cefea83e090448a664482406c6
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-09-15 23:17:51 +0530

    L131

--- resouce-app/src/App.vue
+++ resouce-app/src/App.vue
@@ -1,9 +1,42 @@
-<template>hello world</template>
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    >
+    </learning-resource>
+  </ul>
+</template>
 
 <script>
+import LearningResource from "./components/LearningResource.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guid",
+          title: "Official Guide",
+          description: "The official Vue.js documentation",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to Google....",
+          link: "https://www.google.com",
+        },
+      ],
+    };
+  },
 };
 </script>
 
---
+++ resouce-app/src/components/LearningResource.vue
@@ -0,0 +1,25 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link">View Resource</a>
+    </nav>
+  </li>
+</template>
+;
+
+<script>
+export default {
+  name: "LearningResource",
+  props: ["title", "description", "link"],
+  mounted() {
+    console.log(this.title);
+  },
+};
+</script>
